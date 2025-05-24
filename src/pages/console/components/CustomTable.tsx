088829cbae1c46afdbadda9c0a3ed8b62db7611f
Navojith <navojith22@gmail.com>
2025-05-24 19:06:22 +0530

    feat: deploy

--- src/pages/console/components/CustomTable.tsx
+++ src/pages/console/components/CustomTable.tsx
@@ -1,6 +1,8 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridRowId } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import { CustomTableProps } from '../../../types/common';
+import { useState } from 'react';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 
 const paginationModel = { page: 0, pageSize: 5 };
 
@@ -9,11 +11,38 @@ export default function CustomTable({
   rows,
   autoHeight,
 }: CustomTableProps) {
+  const [allRows, setAllRows] = useState(rows);
+
+  const allHeaders = [
+    ...headers,
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }: { id: GridRowId }) => {
+        return [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setAllRows(rows.filter((row: any) => row?.id !== id));
+  };
+
   return (
     <Paper sx={{ height: 700, width: '100%' }}>
       <DataGrid
-        rows={rows}
-        columns={headers}
+        rows={allRows}
+        columns={allHeaders}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
